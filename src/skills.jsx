9aa82dbb5f028d0264ea86f9b51cdfd5a885fda6
DevSankale <sankaledev@gmail.com>
2024-07-21 17:49:26 +0300

    skills images resizing.

--- src/skills.jsx
+++ src/skills.jsx
@@ -3,12 +3,15 @@ import React from "react"
 export default function Skills(){
 
   return (
+    <>
+    <p className="skill">SKILLS</p>
     <div className="skills">
-    
-    <img src="./images/java-script.png" className="skillimage"/>
-    <img src="./images/react.png" className="skillimage"/>
-    <img src="./images/css.png" className="skillimage"/>
-    <img src="./images/html.png" className="skillimage"/>
+    <p><img src="./images/java-script.png" className="skillimage"/>Javascript</p>
+    <p><img src="./images/react.png" className="skillimage"/>React</p>
+    <p><img src="./images/css.png" className="skillimage"/>CSS</p>
+    <p><img src="./images/html.png" className="skillimage"/>Html</p>
     </div>
+    </>
+  
   )
 }
