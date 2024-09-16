08b43445672e7e2f16fb4681d75b614f3b92d313
DevSankale <sankaledev@gmail.com>
2024-09-16 14:17:25 +0300

    contact form linked to email

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,86 +1,87 @@
 import React from "react";
 
+export default function Form() {
+  const [formData, setFormData] = React.useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    message: ""
+  });
 
-export default function Form (){
 
 
-  const [formData,setFormData] = React.useState({
-
-        firstName:"",
-        lastName:"",
-        email:"",
-        message:""
-
-  })
-  console.log(formData)
-  function handleChange(event){
-    return (
-        setFormData((prevFormData)=>{
-          return({
-            ...prevFormData,
-            [event.target.name]:event.target.value
-          })
-        })
-    )
+  function handleChange(event) {
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [event.target.name]: event.target.value
+    }));
   }
 
-  function handleSubmit(event){
-          event.preventDefault()
-          console.log(formData)
+  
+  function handleSubmit(event) {
+    
+    console.log(formData);
+    
   }
+
   return (
-
     <div>
-        <form className="form" id="Contact" onSubmit={handleSubmit}>
-          <label htmlFor="fisrtName">First Name
-          </label>
-            <br/>
-          <input 
-                type="text" 
-                id="firstName"
-                name="firstName" 
-                required
-                onChange={handleChange}
-                value={formData.firstName}
-          />
-          <br/>
-          <label htmlFor="lastName">Last Name</label>
-          <br/>
-          <input 
-                type="text" 
-                id="lastName" 
-                name="lastName" 
-                required
-                onChange={handleChange}
-                value={formData.lastName}
-          /><br/>
-          <label htmlFor="email">
-            Email
-            </label><br/>
-          <input 
-                type="email" 
-                id="email" 
-                name="email" 
-                required
-                onChange={handleChange}
-                value={formData.email}
-          />
-          <br/><br/>
-          <textarea
-                placeholder="Message Me..." 
-                required
-                onChange={handleChange}
-                name="message"
-                value={formData.message}
-          />
-          <br/>
-          <button 
-                type="submit" 
-                className="js-button">
-                  SUBMIT
-          </button>
-          </form>
-     </div>
-
-  )
+      <form
+        className="form"
+        action="https://formsubmit.co/sankalejoseph@gmail.com"
+        method="POST" 
+        id="Contact"
+        onSubmit={handleSubmit}
+      >
+        <label htmlFor="firstName">First Name</label>
+        <br />
+        <input
+          type="text"
+          id="firstName"
+          name="firstName"
+          required
+          onChange={handleChange}
+          value={formData.firstName}
+        />
+        <br />
+        <label htmlFor="lastName">Last Name</label>
+        <br />
+        <input
+          type="text"
+          id="lastName"
+          name="lastName"
+          required
+          onChange={handleChange}
+          value={formData.lastName}
+        />
+        <br />
+        <label htmlFor="email">Email</label>
+        <br />
+        <input
+          type="email"
+          id="email"
+          name="email"
+          required
+          onChange={handleChange}
+          value={formData.email}
+        />
+        <br /><br />
+        <textarea
+          placeholder="Message Me..."
+          required
+          onChange={handleChange}
+          name="message"
+          value={formData.message}
+        />
+        <br />
+        
+        {/* Optional hidden fields */}
+        <input type="hidden" name="_subject" value="New Contact Form Submission" />
+        
+        <button type="submit" className="js-button">
+          SUBMIT
+        </button>
+      </form>
+    </div>
+  );
 }
